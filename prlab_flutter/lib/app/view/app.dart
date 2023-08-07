c96bca6c9f0ab399181a595660bce4c09d9ad2b0
manu <manunavarro95@hotmail.com>
2023-08-07 12:18:36 -0300

    theme in progress

--- prlab_flutter/lib/app/view/app.dart
+++ prlab_flutter/lib/app/view/app.dart
@@ -24,11 +24,12 @@ class _AppState extends State<App> {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        appBarTheme: const AppBarTheme(color: Color(0xFF13B9FF)),
-        colorScheme: ColorScheme.fromSwatch(
-          accentColor: const Color(0xFF13B9FF),
+      darkTheme: ThemeData.from(colorScheme: ColorScheme.dark()),
+      theme: ThemeData.from( 
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFFA12B46),
         ),
+        useMaterial3: true,
       ),
       builder: (context, child) => ScrollConfiguration(
         behavior: NoGlowBehavior(),
