78a7b69e22c6b15f3ebf10652d5dcbc09a619331
chivo <rodrigo.m.quintero@gmail.com>
2023-08-11 10:59:59 -0300

    cambios

--- prlab_server/lib/src/repositories/auth_repository.dart
+++ prlab_server/lib/src/repositories/auth_repository.dart
@@ -1,13 +1,22 @@
 import 'package:serverpod/server.dart';
 
-class AuthRepository{
-  Future<String> getValidationCode({required Session session, required String email}) async {
-    var result = await session.db.query(
-        'SELECT "verificationCode" FROM serverpod_email_create_request WHERE email = \'$email\'');
-    if (result.isEmpty) {
-      return 'Email not found';
-    } else {
-      return result.first.first;
+class AuthRepository {
+  //trae de la tabla serverpod_email_create_request el codigo de verificacion
+  // segun el email que se le pase
+  Future<String> getValidationCode({
+    required Session session,
+    required String email,
+  }) async {
+    try {
+      final result = await session.db.query(
+          'SELECT "verificationCode" FROM serverpod_email_create_request WHERE email = \'$email\'');
+      if (result.isEmpty) {
+        return 'Email not found';
+      } else {
+        return result.first.first;
+      }
+    } catch (e) {
+      rethrow;
     }
   }
 }
--- prlab_server/lib/src/services/auth_service.dart
+++ prlab_server/lib/src/services/auth_service.dart
@@ -2,8 +2,10 @@ import 'package:prlab_server/src/repositories/auth_repository.dart';
 import 'package:serverpod/serverpod.dart';
 
 class AuthService {
-  Future<String> getValidationCode(
-      {required Session session, required String email}) async {
+  Future<String> getValidationCode({
+    required Session session,
+    required String email,
+  }) async {
     return await AuthRepository()
         .getValidationCode(session: session, email: email);
   }
