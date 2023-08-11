d3b1760722a8c6c0a056fe452c8056f729210e7f
chivo <rodrigo.m.quintero@gmail.com>
2023-08-11 13:24:33 -0300

    correcciones generales

--- prlab_server/lib/src/repositories/auth_repository.dart
+++ prlab_server/lib/src/repositories/auth_repository.dart
@@ -1,8 +1,21 @@
 import 'package:serverpod/server.dart';
 
+/// La clase AuthRepository es responsable de manejar las operaciones contra la DB
+///  relacionadas con la autenticación.
 class AuthRepository {
-  //trae de la tabla serverpod_email_create_request el codigo de verificacion
-  // segun el email que se le pase
+
+  /// La función `getValidationCode` recupera el código de verificación asociado con un correo
+  /// electrónico determinado de la tabla serverpod_email_create_request de la base de datos.
+  /// 
+  /// Args:
+  ///   session (Session): El parámetro `session` es del tipo `Session` y es obligatorio. Representa la
+  /// sesión o conexión actual a la base de datos.
+  ///   email (String): El parámetro de correo electrónico es una cadena obligatoria que representa la
+  /// dirección de correo electrónico para la que se recupera el código de validación.
+  /// 
+  /// retorna un `Future<String>` (el codigo en si).
+ 
+ 
   Future<String> getValidationCode({
     required Session session,
     required String email,
--- prlab_server/lib/src/services/auth_service.dart
+++ prlab_server/lib/src/services/auth_service.dart
@@ -1,12 +1,25 @@
 import 'package:prlab_server/src/repositories/auth_repository.dart';
 import 'package:serverpod/serverpod.dart';
 
+final authRepository = AuthRepository();
+
 class AuthService {
+  /// La función `getValidationCode` devuelve un Future que recupera un código de validación del
+  /// `AuthRepository` mediante una sesión y un correo electrónico.
+  ///
+  /// Args:
+  ///   session (Session): Un parámetro requerido de tipo Sesión. Representa la sesión de usuario actual
+  /// y es necesaria para fines de autenticación.
+  ///   email (String): El parámetro de correo electrónico es una cadena obligatoria que representa la
+  /// dirección de correo electrónico para la que se solicita el código de validación.
+  ///
+  /// Returns:
+  ///   El método devuelve un `Future<String>`.
   Future<String> getValidationCode({
     required Session session,
     required String email,
   }) async {
-    return await AuthRepository()
-        .getValidationCode(session: session, email: email);
+    return await authRepository.getValidationCode(
+        session: session, email: email);
   }
 }
