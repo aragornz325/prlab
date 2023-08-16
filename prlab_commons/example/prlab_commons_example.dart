1038a2ba3f4e53045e7a8418bf91393494612010
Juan José García <juanjgar998@gmail.com>
2023-08-16 16:37:37 -0300

    Borrar archivos y directorios sobrantes en prlab_commons.

--- prlab_commons/example/prlab_commons_example.dart
+++
@@ -1,6 +0,0 @@
-import 'package:prlab_commons/prlab_commons.dart';
-
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
--- prlab_commons/lib/prlab_commons.dart
+++ prlab_commons/lib/prlab_commons.dart
@@ -3,7 +3,6 @@
 /// More dartdocs go here.
 library;
 
-export 'src/prlab_commons_base.dart';
 export 'modelos/modelos.dart';
 
 // TODO: Export any libraries intended for clients of this package.
