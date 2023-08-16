aeb4c6ce02419dc8933e5ee600c24ecd5538ee39
AndreasMeggetto <meggeto.andreas@c-ven.com>
2023-08-16 09:29:24 -0300

    Formatear el TextInputFormatter de fecha

--- prlab_flutter/lib/utilidades/extensions/formateador_de_fecha.dart
+++ prlab_flutter/lib/utilidades/extensions/formateador_de_fecha.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/services.dart';
 
-// TODO(Andreas):
-// Arreglar este validador de fechas, la idea es hacer un validador
-// que solamente permita al usuario poner fechas validas
 class FormateadorDeFecha extends TextInputFormatter {
   @override
   TextEditingValue formatEditUpdate(
@@ -17,24 +14,30 @@ class FormateadorDeFecha extends TextInputFormatter {
       if (newValue.text.length > 10) return oldValue;
 
       if (newValue.text.length == 2 || newValue.text.length == 5) {
+        final valorFormateado = '${newValue.text}/';
+
         return TextEditingValue(
-          text: '${newValue.text}/',
+          text: valorFormateado,
           selection: TextSelection.collapsed(
             offset: newValue.selection.end + 1,
           ),
         );
       } else if (newValue.text.length == 3 && newValue.text[2] != '/') {
+        final valorFormateado =
+            '${newValue.text.substring(0, 2)}/${newValue.text.substring(2)}';
+
         return TextEditingValue(
-          text:
-              '${newValue.text.substring(0, 2)}/${newValue.text.substring(2)}',
+          text: valorFormateado,
           selection: TextSelection.collapsed(
             offset: newValue.selection.end + 1,
           ),
         );
       } else if (newValue.text.length == 6 && newValue.text[5] != '/') {
+        final valorFormateado =
+            '${newValue.text.substring(0, 5)}/${newValue.text.substring(5)}';
+
         return TextEditingValue(
-          text:
-              '${newValue.text.substring(0, 5)}/${newValue.text.substring(5)}',
+          text: valorFormateado,
           selection: TextSelection.collapsed(
             offset: newValue.selection.end + 1,
           ),
@@ -45,6 +48,7 @@ class FormateadorDeFecha extends TextInputFormatter {
         RegExp('[^0-9]').hasMatch(newValue.text)) {
       return oldValue;
     }
+
     return newValue;
   }
 }
