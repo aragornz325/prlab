873e3cea40aa22c9d3b28f1d70b538ff136287c0
Matias Sanchez <107004008+MatuCorp@users.noreply.github.com>
2023-08-24 17:36:32 -0300

    documente el bloc

--- prlab_flutter/lib/paginas/administracion_contenido/bloc/bloc_administracion_contenido_evento.dart
+++ prlab_flutter/lib/paginas/administracion_contenido/bloc/bloc_administracion_contenido_evento.dart
@@ -1,5 +1,9 @@
 part of 'bloc_administracion_contenido.dart';
 
-//TODO(mati): hacer documentacion
+/// {@template BlocAdministracionContenidoEvento}
+/// Maneja los distintos eventos que pueden filtrar de distintas formas los
+/// artículos,por nombre en un textfield/campo de texto o filtrar por fecha y
+/// el estado de un artículo.
+/// {@endtemplate}
 @immutable
 abstract class BlocAdministracionContenidoEvento {}
