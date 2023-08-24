1c87457a48b81304cbd044c06d14bb02684fc9a1
Matias Sanchez <107004008+MatuCorp@users.noreply.github.com>
2023-08-24 17:17:37 -0300

    terminado lo de la lista falta separar el componente y documentar(arreglar lo del datetime)

--- prlab_flutter/lib/paginas/administracion_contenido/escritorio/widgets/contenido_principal.dart
+++ prlab_flutter/lib/paginas/administracion_contenido/escritorio/widgets/contenido_principal.dart
@@ -2,11 +2,10 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:full_responsive/full_responsive.dart';
-import 'package:intl/intl.dart';
 import 'package:prlab_flutter/extensiones/extensiones.dart';
 import 'package:prlab_flutter/paginas/administracion_contenido/escritorio/widgets/widgets.dart';
 
-import '../../../../widgets/widgets.dart';
+import 'package:prlab_flutter/widgets/widgets.dart';
 
 // TODO(mati): hacer documentacion
 class ContenidoPrincipal extends StatelessWidget {
@@ -25,67 +24,37 @@ class ContenidoPrincipal extends StatelessWidget {
     var listaPrArticuls = [
       PRArticulo(
         fecha: DateTime.now(),
-        nombre: 'louka',
+        nombre: 'Flutter article',
         status: 'Draft',
         tieneAutor: false,
       ),
       PRArticulo(
         fecha: DateTime.now(),
-        nombre: 'andre',
+        nombre: 'Flutter article',
         status: 'Draft',
         tieneAutor: false,
       ),
       PRArticulo(
         fecha: DateTime.now(),
-        nombre: 'andre',
+        nombre: 'Flutter article',
         status: 'Draft',
         tieneAutor: false,
       ),
       PRArticulo(
         fecha: DateTime.now(),
-        nombre: 'andre',
+        nombre: 'Flutter article',
         status: 'Draft',
         tieneAutor: false,
       ),
       PRArticulo(
         fecha: DateTime.now(),
-        nombre: 'andre',
+        nombre: 'Flutter article',
         status: 'Draft',
         tieneAutor: false,
       ),
       PRArticulo(
         fecha: DateTime.now(),
-        nombre: 'andre',
-        status: 'Draft',
-        tieneAutor: false,
-      ),
-      PRArticulo(
-        fecha: DateTime.now(),
-        nombre: 'andre',
-        status: 'Draft',
-        tieneAutor: false,
-      ),
-      PRArticulo(
-        fecha: DateTime.now(),
-        nombre: 'andre',
-        status: 'Draft',
-        tieneAutor: false,
-      ),
-      PRArticulo(
-        fecha: DateTime.now(),
-        nombre: 'andre',
-        status: 'Draft',
-        tieneAutor: false,
-      ),
-      PRArticulo(
-        fecha: DateTime.now(),
-        nombre: 'andre',
-        status: 'Draft',
-        tieneAutor: false,
-      ),
-      PRArticulo(
-        fecha: DateTime.now(),
-        nombre: 'andre',
+        nombre: 'Flutter article',
         status: 'Draft',
         tieneAutor: false,
       ),
@@ -108,25 +77,30 @@ class ContenidoPrincipal extends StatelessWidget {
                 Divider(color: colores.onSecondary),
                 const TextFieldBusquedaFiltrado(),
                 Divider(color: colores.onSecondary),
-                // const ListaArticulos(tieneAutor: true),
+                // TODO(mati): cambiar/separar lo de grilla a un/a nueva/widget (clase statelessWidget) por el de lista de articulo y hacer que reciba una lista del modelo articulo(del back)
                 Grilla(
                   listaModelos: [
                     // nose como funciona,pero funciona
                     // Articulo
                     ModeloLista<String>(
-                      widthDeLaColumna: 300.pw,
+                      widthDeLaColumna: 400.pw,
                       lista: listaPrArticuls.map((e) => e.nombre).toList(),
                       nombreColumna: 'Article',
-                      celdaEncabezadoColumna: (value) => SizedBox(
-                        width: 300.pw,
-                        child: Padding(
-                          padding: EdgeInsets.only(left: 30.pw),
-                          child: Text(
-                            value,
-                            style: TextStyle(
-                              color: colores.primary,
-                              fontSize: 16.pf,
-                              fontWeight: FontWeight.w600,
+                      celdaEncabezadoColumna: (value) => Padding(
+                        padding: EdgeInsets.symmetric(
+                          vertical: max(10.ph, 10.sh),
+                        ),
+                        child: SizedBox(
+                          width: 400.pw,
+                          child: Padding(
+                            padding: EdgeInsets.only(left: 30.pw),
+                            child: Text(
+                              value,
+                              style: TextStyle(
+                                color: colores.primary,
+                                fontSize: 16.pf,
+                                fontWeight: FontWeight.w600,
+                              ),
                             ),
                           ),
                         ),
@@ -136,13 +110,13 @@ class ContenidoPrincipal extends StatelessWidget {
                           children: [
                             Container(
                               padding: EdgeInsets.only(left: 30.pw),
-                              width: 300.pw,
+                              width: 400.pw,
                               height: max(80.ph, 80.sh),
                               child: Column(
                                 children: [
                                   SizedBox(
-                                    width: 300.pw,
-                                    height: max(30.ph, 30.sh),
+                                    width: 400.pw,
+                                    height: max(50.ph, 50.sh),
                                     child: Text(
                                       value.toString(),
                                       maxLines: 1,
@@ -168,10 +142,11 @@ class ContenidoPrincipal extends StatelessWidget {
                     // TODO(anyone): cambiar por el enum del modelo
                     // Status
                     ModeloLista<String>(
+                      widthDeLaColumna: 150.pw,
                       lista: listaPrArticuls.map((e) => e.status).toList(),
                       nombreColumna: 'Status',
                       celdaEncabezadoColumna: (value) => SizedBox(
-                        width: 100.pw,
+                        width: 150.pw,
                         child: Center(
                           child: Text(
                             value,
@@ -185,28 +160,34 @@ class ContenidoPrincipal extends StatelessWidget {
                       ),
                       celda: <String>(value) {
                         return SizedBox(
-                          width: 100.pw,
+                          width: 150.pw,
                           height: max(80.ph, 80.sh),
                           child: Column(
                             children: [
-                              Container(
-                                width: 100.pw,
-                                height: max(30.ph, 30.sh),
-                                decoration: const BoxDecoration(
-                                  // TODO(mati): cambiar color al del theme
-                                  color: Colors.grey,
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(100)),
-                                ),
+                              SizedBox(
+                                height: max(50.ph, 50.sh),
                                 child: Center(
-                                  child: Text(
-                                    value.toString(),
-                                    maxLines: 1,
-                                    overflow: TextOverflow.ellipsis,
-                                    style: TextStyle(
-                                      color: colores.onPrimary,
-                                      fontSize: 15.pf,
-                                      fontWeight: FontWeight.w500,
+                                  child: Container(
+                                    width: 100.pw,
+                                    height: max(35.ph, 35.sh),
+                                    decoration: const BoxDecoration(
+                                      // TODO(mati): cambiar color al del theme
+                                      color: Colors.grey,
+                                      borderRadius: BorderRadius.all(
+                                        Radius.circular(100),
+                                      ),
+                                    ),
+                                    child: Center(
+                                      child: Text(
+                                        value.toString(),
+                                        maxLines: 1,
+                                        overflow: TextOverflow.ellipsis,
+                                        style: TextStyle(
+                                          color: colores.onPrimary,
+                                          fontSize: 15.pf,
+                                          fontWeight: FontWeight.w500,
+                                        ),
+                                      ),
                                     ),
                                   ),
                                 ),
@@ -221,10 +202,11 @@ class ContenidoPrincipal extends StatelessWidget {
 
                     // Fecha
                     ModeloLista<DateTime>(
+                      widthDeLaColumna: 150.pw,
                       lista: listaPrArticuls.map((e) => e.fecha).toList(),
                       nombreColumna: 'Last Date',
                       celdaEncabezadoColumna: (value) => SizedBox(
-                        width: 100.pw,
+                        width: 150.pw,
                         child: Center(
                           child: Text(
                             value,
@@ -238,16 +220,18 @@ class ContenidoPrincipal extends StatelessWidget {
                       ),
                       celda: <DateTime>(DateTime value) {
                         return SizedBox(
-                          width: 100.pw,
+                          width: 150.pw,
                           height: max(80.ph, 80.sh),
                           child: Column(
                             children: [
                               SizedBox(
-                                width: 100.pw,
-                                height: max(30.ph, 30.sh),
+                                width: 150.pw,
+                                height: max(50.ph, 50.sh),
                                 child: Center(
                                   child: Text(
-                                    DateFormat.yMd().format(value),
+                                    // TODO(mati): pasarle la fecha al text
+                                    '01 Aug 2023',
+                                    // DateFormat.yMd().format(value),
                                     maxLines: 1,
                                     overflow: TextOverflow.ellipsis,
                                     textAlign: TextAlign.start,
@@ -268,12 +252,13 @@ class ContenidoPrincipal extends StatelessWidget {
 
                     // Autor
                     ModeloLista<String>(
+                      widthDeLaColumna: 150.pw,
                       lista: listaPrArticuls
                           .map((e) => e.urlImageAutor.toString())
                           .toList(),
                       nombreColumna: 'Author',
                       celdaEncabezadoColumna: (value) => SizedBox(
-                        width: 100.pw,
+                        width: 150.pw,
                         child: Center(
                           child: Text(
                             value,
@@ -287,13 +272,13 @@ class ContenidoPrincipal extends StatelessWidget {
                       ),
                       celda: <String>(value) {
                         return SizedBox(
-                          width: 100.pw,
+                          width: 150.pw,
                           height: max(80.ph, 80.sh),
                           child: Column(
                             children: [
                               SizedBox(
-                                width: 100.pw,
-                                height: max(30.ph, 30.sh),
+                                width: 150.pw,
+                                height: max(50.ph, 50.sh),
                                 child: Center(
                                   child: CircleAvatar(
                                     radius: 15.pf,
@@ -314,10 +299,11 @@ class ContenidoPrincipal extends StatelessWidget {
 
                     // botones
                     ModeloLista<String>(
+                      widthDeLaColumna: 150.pw,
                       lista: listaPrArticuls.map((e) => e.status).toList(),
                       nombreColumna: '',
                       celdaEncabezadoColumna: (value) => SizedBox(
-                        width: 100.pw,
+                        width: 150.pw,
                         child: Text(
                           value,
                           style: TextStyle(
@@ -329,23 +315,38 @@ class ContenidoPrincipal extends StatelessWidget {
                       ),
                       celda: <String>(value) {
                         return SizedBox(
-                          width: 100.pw,
+                          width: 150.pw,
                           height: max(80.ph, 80.sh),
                           child: Column(
                             children: [
-                              Container(
-                                height: max(30.ph, 30.sh),
-                                child: Center(
-                                  child: Text(
-                                    '',
-                                    maxLines: 1,
-                                    overflow: TextOverflow.ellipsis,
-                                    style: TextStyle(
-                                      color: colores.onPrimary,
-                                      fontSize: 15.pf,
-                                      fontWeight: FontWeight.w500,
+                              SizedBox(
+                                width: 150.pw,
+                                height: max(50.ph, 50.sh),
+                                child: Row(
+                                  children: [
+                                    const Spacer(),
+                                    IconButton(
+                                      onPressed: () {
+                                        // TODO(anyone): agregarle funcionalidad
+                                      },
+                                      icon: Icon(
+                                        Icons.share_outlined,
+                                        size: 18.pf,
+                                        color: colores.primary,
+                                      ),
                                     ),
-                                  ),
+                                    IconButton(
+                                      onPressed: () {
+                                        // TODO(anyone): agregarle funcionalidad
+                                      },
+                                      icon: Icon(
+                                        Icons.more_vert_outlined,
+                                        size: 18.pf,
+                                        color: colores.primary,
+                                      ),
+                                    ),
+                                    SizedBox(width: 10.pw),
+                                  ],
                                 ),
                               ),
                               Divider(color: colores.onSecondary),
