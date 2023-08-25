a6f7bf16ca47f2b2d091f7548e195a7832a38826
Juan José García <juanjgar998@gmail.com>
2023-08-25 17:19:41 -0300

    Comentar otros modelos.

--- prlab_client/lib/src/modelos/base/base.dart
+++ prlab_client/lib/src/modelos/base/base.dart
@@ -1,18 +1,18 @@
-import 'package:prlab_client/prlab_client.dart';
+// import 'package:prlab_client/prlab_client.dart';
 
-/// Modelo de entidad abstracta Base (contiene elementos comunes a todos los modelos).
-/// Extiende de `TableRow`, clase de Serverpod para manipular objetos que se corresponden
-/// con entidades en la Base de Datos.
-abstract class ModeloBase extends SerializableEntity {
-  ModeloBase({
-    int? id,
-    this.fechaCreacion,
-  });
+// /// Modelo de entidad abstracta Base (contiene elementos comunes a todos los modelos).
+// /// Extiende de `TableRow`, clase de Serverpod para manipular objetos que se corresponden
+// /// con entidades en la Base de Datos.
+// abstract class ModeloBase extends SerializableEntity {
+//   ModeloBase({
+//     int? id,
+//     this.fechaCreacion,
+//   });
 
-  int? id;
+//   int? id;
 
-  /// Es la fecha (timestamp) en la que se inserta el objeto en la Base de Datos. Puede 
-  /// tener un valor nulo cuando se crea el objeto y el valor se asigna en la Base de 
-  /// Datos.
-  DateTime? fechaCreacion;
-}
+//   /// Es la fecha (timestamp) en la que se inserta el objeto en la Base de Datos. Puede 
+//   /// tener un valor nulo cuando se crea el objeto y el valor se asigna en la Base de 
+//   /// Datos.
+//   DateTime? fechaCreacion;
+// }
--- prlab_client/lib/src/modelos/entregable/entregable.dart
+++ prlab_client/lib/src/modelos/entregable/entregable.dart
@@ -1,26 +1,26 @@
-import 'package:prlab_client/src/modelos/base/base.dart';
+// import 'package:prlab_client/src/modelos/base/base.dart';
 
-/// Modelo de entidad abstracta Entregable (Extiende a otras clases como Publicacion, Curso, etc
-/// con sus atributos comunes).
-abstract class Entregable extends ModeloBase {
-  Entregable({
-    super.id,
-    required this.nombre,
-    required this.idAutor,
-    this.fechaInicio,
-    this.fechaFin,
-    super.fechaCreacion,
-  });
+// /// Modelo de entidad abstracta Entregable (Extiende a otras clases como Publicacion, Curso, etc
+// /// con sus atributos comunes).
+// abstract class Entregable extends ModeloBase {
+//   Entregable({
+//     super.id,
+//     required this.nombre,
+//     required this.idAutor,
+//     this.fechaInicio,
+//     this.fechaFin,
+//     super.fechaCreacion,
+//   });
 
-  /// Nombre/titulo del Entregable.
-  String? nombre;
+//   /// Nombre/titulo del Entregable.
+//   String? nombre;
 
-  /// ID del usuario que crea el Entregable.
-  int? idAutor;
+//   /// ID del usuario que crea el Entregable.
+//   int? idAutor;
 
-  /// Fecha de inicio del Entregable.
-  DateTime? fechaInicio;
+//   /// Fecha de inicio del Entregable.
+//   DateTime? fechaInicio;
 
-  /// Fecha de finalizacion o plazo del Entregable.
-  DateTime? fechaFin;
-}
+//   /// Fecha de finalizacion o plazo del Entregable.
+//   DateTime? fechaFin;
+// }
