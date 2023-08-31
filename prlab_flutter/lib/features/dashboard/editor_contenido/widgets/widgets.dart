2aa3592f37bee583d4e7872d5e44e13a655cec9e
AndreasMeggetto <meggeto.andreas@c-ven.com>
2023-08-31 10:26:52 -0300

    Agregar export de widgets en editar contenido

--- prlab_flutter/lib/features/dashboard/editor_contenido/widgets/widgets.dart
+++ prlab_flutter/lib/features/dashboard/editor_contenido/widgets/widgets.dart
@@ -2,3 +2,4 @@ export 'articulo_prlab.dart';
 export 'container_edicion_articulo.dart';
 export 'editor_de_descripcion_de_contenido.dart';
 export 'paginas_del_articulo.dart';
+export 'upload_logo_pr.dart';
