3fdc9a624c3a1675de49a10e932bd7d3f0b61eba
Manuel Navarro <manunavarro95@hotmail.com>
2023-09-12 17:07:41 +0000

    Resolve "[WDG] Tarjeta articulos recientes"

---
+++ prlab_flutter/lib/utilidades/widgets/card_articulo_reciente.dart/modelo_articulo.dart
@@ -0,0 +1,12 @@
+class Articulo {
+  Articulo({
+    required this.titulo,
+    required this.imagen,
+    required this.fechaCreacion,
+    required this.urlArticulo,
+  });
+  String titulo;
+  String imagen;
+  DateTime fechaCreacion;
+  String urlArticulo;
+}
