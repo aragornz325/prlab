aeaaf3b5adabd880d0670286e0323aa4bd029580
Juan José García <juanjgar998@gmail.com>
2023-09-22 15:01:04 -0300

    Agregar temas e idiomas al modelo de periodista y la query.

--- prlab_server/lib/src/odms/odm_periodista.dart
+++ prlab_server/lib/src/odms/odm_periodista.dart
@@ -24,8 +24,8 @@ class OdmPeriodista extends ODM {
     final queryCiudades = idCiudades.isNotEmpty
         ? 'j."idCiudad" IN (${idCiudades.join(',')})'
         : '';
-    // final queryTemas = idTemas.isNotEmpty ? '"idTema" IN (${idTemas.join(',')})' : '';
-    // final queryIdiomas = idIdiomas.isNotEmpty ? '"idIdioma" IN (${idIdiomas.join(',')})' : '';
+    final queryTemas = idTemas.isNotEmpty ? 't."id" IN (${idTemas.join(',')})' : '';
+    final queryIdiomas = idIdiomas.isNotEmpty ? 'l."id" IN (${idIdiomas.join(',')})' : '';
     final queryTiposDeMedio = idTiposDeMedio.isNotEmpty
         ? 'm."idTipoDeMedio" IN (${idTiposDeMedio.join(',')})'
         : '';
@@ -37,8 +37,10 @@ class OdmPeriodista extends ODM {
       queryNombreDeMedio,
       queryPaises,
       queryCiudades,
-      queryRoles,
+      queryTemas,
+      queryIdiomas,
       queryTiposDeMedio,
+      queryRoles,
     ].where((element) => (element != '' && element != [])).toList();
 
     var whereBuffer = StringBuffer();
@@ -52,6 +54,7 @@ class OdmPeriodista extends ODM {
           whereBuffer.writeln('   AND ${listaWheres[i]}');
         }
       }
+      whereBuffer.writeln();
     }
 
     final modeloApi = Periodista().toJson()
@@ -61,13 +64,41 @@ class OdmPeriodista extends ODM {
       ..remove('idPais');
 
     var consulta = '''
-SELECT j."id", j."urlImagen", j."nombreCompleto", r."rol", m."medio", j."bio", j."email", j."telefono", c."ciudad", n."pais", j."redesSociales"
+SELECT 
+  j."id", 
+  j."urlImagen", 
+  j."nombreCompleto", 
+  r."rol", 
+  m."medio", 
+  j."bio", 
+  (
+        SELECT ARRAY_AGG(t."tema" ORDER BY t."id")
+        FROM temas_periodistas tj
+        LEFT JOIN temas t ON tj."idTema" = t."id"
+        WHERE tj."idPeriodista" = j."id"
+    ) AS "temas", 
+  j."email", 
+  j."telefono", 
+  c."ciudad", 
+  n."pais", 
+  (
+        SELECT ARRAY_AGG(l."idioma" ORDER BY l."id")
+        FROM idiomas_periodistas lj
+        LEFT JOIN idiomas l ON lj."idIdioma" = l."id"
+        WHERE lj."idPeriodista" = j."id"
+    ) AS "idiomas", 
+  j."redesSociales"
 FROM
   periodistas j
   INNER JOIN roles r ON j."idRol" = r.id
   INNER JOIN medios m ON j."idMedio" = m.id
   INNER JOIN ciudades c ON j."idCiudad" = c.id
-  INNER JOIN paises n ON j."idPais" = n.id$whereBuffer;
+  INNER JOIN paises n ON j."idPais" = n.id
+  LEFT JOIN temas_periodistas tj ON j."id" = tj."idPeriodista"
+  LEFT JOIN temas t ON tj."idTema" = t."id"
+  LEFT JOIN idiomas_periodistas lj ON j."id" = lj."idPeriodista"
+  LEFT JOIN idiomas l ON lj."idIdioma" = l."id"$whereBuffer
+  GROUP BY j."id", r."rol", m."medio", c."ciudad", n."pais";
     ''';
     final response = await ejecutarConsultaSql(
       session,
@@ -76,4 +107,5 @@ FROM
     );
     return response.map((e) => Periodista.fromJson(e, Protocol())).toList();
   }
+
 }
